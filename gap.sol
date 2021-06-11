41416166052a0772c33eb3852fa1b0d75a3c1212
gapbsc <85207922+gapbsc@users.noreply.github.com>
2021-06-11 13:42:00 -0300

    Update gap.sol

--- gap.sol
+++ gap.sol
@@ -1,4 +1,8 @@
 /**
+ *Submitted for verification at BscScan.com on 2021-06-06
+*/
+
+/**
 Global Adversity Project (GAP) is a yield-generating charitable cryptocurrency that runs on the Binance Smart Network. 
 It rewards holders by providing them ‘interest’ on their investment through the contract transaction tax, offering an 
 incentive for investors to hold, rather then participating in day-trading events.
